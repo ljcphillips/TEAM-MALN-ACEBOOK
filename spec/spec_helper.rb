0e2225c8e50a1e01f0eae4b4ecf5b6e9db40ac0c
Lorraine Philllips <ljcphillips@gmail.com>
2018-05-17 12:33:08 +0100

    added votable gem and migrated

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,6 +22,7 @@ SimpleCov.start
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
 RSpec.configure do |config|
   # config.include Warden::Test::ControllerHelpers, type: :controller
   #
