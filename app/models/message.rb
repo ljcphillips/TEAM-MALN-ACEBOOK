8b0f7ff0b122ae57e2f5acecaf4260fe6511185c
marydomashneva <mary.domashneva@gmail.com>
2018-05-17 22:46:24 +0100

    User can post on the wall

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :user
+  belongs_to :recipient_user, class_name: 'User', foreign_key: 'recipient_user_id'
+
+  def author
+    return user
+  end
 end
